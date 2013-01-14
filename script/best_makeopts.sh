c9ea94258dab8d9f25e28a392af0ea3b6bbd49c0
Agostino Sarubbo <ago@gentoo.org>
2013-01-14 11:20:10 +0100

    Be sure you don't use strange EMERGE_DEFAULT_OPTS

--- script/best_makeopts.sh
+++ script/best_makeopts.sh
@@ -9,6 +9,6 @@ DISTDIR="/tmp/" emerge -f ${PACKAGE}
 for i in {1..10}
 do
 	echo 1 > /proc/sys/vm/drop_caches
-	time DISTDIR="/tmp" MAKEOPTS="-j${i}" emerge -q1OB ${PACKAGE}
+	time DISTDIR="/tmp" EMERGE_DEFAULT_OPTS="" MAKEOPTS="-j${i}" emerge -q1OB ${PACKAGE}
 	echo -ne "\n\n\n"
 done
